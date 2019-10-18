9d55d955a9916b8bd7ee2b130e10f753a454fcb9
zhongjunqi <zhongjunqi@iplas.com.cn>
2019-10-18 18:02:26 +0800

    upload demo

---
+++ precache-manifest.5cc9c8e80bfcc43a13ee4c87d75a24cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "75c3e5cff229556299c1",
+    "url": "js/chunk-vendors.68958384.js"
+  },
+  {
+    "revision": "2ac4400e341e39e2b1cc",
+    "url": "js/app.d14cf605.js"
+  },
+  {
+    "revision": "8e66c036dc547a4e5312",
+    "url": "js/about.efd6da9e.js"
+  },
+  {
+    "revision": "bca4e612dd06812777006716f26e1d14",
+    "url": "index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "2ac4400e341e39e2b1cc",
+    "url": "css/app.3c0b035c.css"
+  }
+];
